2b47854871de4d8e8bc62ee81c64f66e8646ce1a
Wahyu Kurniawan <ayungavis@gmail.com>
2023-01-31 14:50:02 +0700

    🔧 chore: update react to v18

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("app")
-);
+const container = document.getElementById("app");
+if (container !== null) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
